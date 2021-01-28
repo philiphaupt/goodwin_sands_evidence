1cac424813d654b6778918709cd11687c19a3998
philiphaupt <philip.haupt@gmail.com>
2021-01-28 09:15:13 +0000

    add ggplot2 for sf object map and colour scheme that mathces the results report using ggdark theme

--- scripts/solution_map.R
+++ scripts/solution_map.R
@@ -6,31 +6,46 @@ library(mapview)
 # read in for plotting
 KEIFCA_boundary_line <- st_read("C:/Users/Phillip Haupt/Documents/GIS/6M_12M_from_territorial_sea_baseline_5_Jun_20_and_1983/KEIFCA_6NM.GPKG", layer = "KEIFCA_bounbdary_line")
 
+sol_sf <- s_min_set_sf#s_max_feat_sf
+# SOLUTIONS --------------------------------------------------------
 
-# MINSET SOLUTIONS --------------------------------------------------------
+sol_sf %<>% 
+        dplyr::group_by(solution_1) %>%
+        dplyr::mutate(solution_label = factor(ifelse(solution_1 == 1, "Included", "Excluded")))
+# define colour scheme for included and excluded from solution:
+colour_solution <- c("Included" = "#66a61e", "Excluded" = "#C0C0C0")#ffff99
+
+# plot a map
+ggplot2::ggplot(sol_sf, aes(fill = solution_label))+
+        geom_sf()+
+        labs(fill = "Solution status")+
+        scale_color_manual(values = colour_solution, aesthetics =  "fill")+
+        ggdark::dark_theme_light()
+        
+
+# plot(sol_sf["solution_1"], 
+#      main = "Solution",
+#      axes = TRUE, 
+#      key.width = lcm(1.3), 
+#      key.length = 1.0, 
+#      key.pos = 4,
+#      pal = sf.colors(2),
+#      graticule = TRUE,
+#      col = colour_solution#ffff99
+#      
+#      #ylim = c(390000, 411390), xlim = c(566500, 569000))
+# )
 
 
-plot(s_min_set_sf["solution_1"], 
-     main = "Solution",
-     axes = TRUE, 
-     key.width = lcm(1.3), 
-     key.length = 1.0, 
-     key.pos = 4,
-     pal = sf.colors(2),
-     graticule = TRUE
-     #ylim = c(390000, 411390), xlim = c(566500, 569000))
-)
-
-
-mapview(s_min_set_sf["solution_1"], 
+mapview(sol_sf["solution_1"], 
         legend = TRUE)
 
 
 # only solution
-sol <- s_min_set_sf %>% dplyr::filter(solution_1 == 1)
+sol_only <- sol_sf %>% dplyr::filter(solution_1 == 1)
 
 # plot solution over the features
-mapview::mapview(list(hab, KEIFCA_boundary_line, sol, sab_and_mussels, agg_area),#KEIFCA read in on vessel_sightings_in_goodwin.R script
+mapview::mapview(list(hab, KEIFCA_boundary_line, sol_only, sab_and_mussels, agg_area),#KEIFCA read in on vessel_sightings_in_goodwin.R script
                  zcol = list("orig_hab","IFCA", "solution_1", "taxonname", "FID"),
                  alpha.regions = list(0.7,0, 0, 1, 0),
                  alpha = list(0.1, 0.7, 1, 0.1, 1),
