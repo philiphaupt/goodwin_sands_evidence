699229a34aabe03a5ceea06e32c78a92f78a5721
philiphaupt <philip.haupt@gmail.com>
2021-01-28 09:15:45 +0000

    produce barchart of target for conservation features.

--- scripts/targets_set.R
+++ scripts/targets_set.R
@@ -1,5 +1,6 @@
 # AIM: Set conservation targets for conservation features
-
+library(ggdark)
+library(tidyverse)
 # The output object is called: "spec" 
 
 # and it is made for the conservation features (so those needs to be read in already) and then defining a proportion of the abundance or area that you want to conserve.
@@ -23,3 +24,27 @@ prop <- c(0.025,0.02, 0.2, 0, 0.2, 0.035) # target is zero for non-designated mi
 spec <- as.data.frame(cbind(id, name, prop))
 spec$id <- as.numeric(spec$id) # make sure that id is numeric (as required by PrioritizeR)
 spec$prop <- as.numeric(spec$prop) # as above
+
+
+source("./scripts/feature_full_names_for_plots.R", echo = T)
+
+print(feature_full_names)
+
+# plot targets and total available per habitat in study area
+
+
+
+puvf %>% dplyr::select(-pu) %>% 
+  group_by(species) %>% 
+  summarise(total_amount = sum(amount)) %>% 
+  right_join(feature_full_names, by = c("species" = "id")) %>% 
+  ggplot(aes(x = prop, y = feature_name, fill = feature_name, label = round(total_amount),0))+
+  geom_col()+
+  xlim(0, 1) +
+  dark_theme_gray() +
+  ggtitle("Conservation targets: Proportion of features required to meet targets set and total available amount per species.")+
+  labs(x = "Porpotion", y = "Feature")+
+  theme(legend.position = "none")+
+  geom_vline(aes(xintercept = 1))+
+  geom_text(x = 0.92)
+
