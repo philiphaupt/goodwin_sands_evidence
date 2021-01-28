954f36af9f6c9faf15bfb8a260d6a1f64c2a77bc
philiphaupt <philip.haupt@gmail.com>
2021-01-28 09:10:50 +0000

    add gear table and few tweaks to map output

--- scripts/cost_prep_vessel_sightings_in_goodwin.R
+++ scripts/cost_prep_vessel_sightings_in_goodwin.R
@@ -17,6 +17,12 @@ boat_sightings <- readxl::read_xlsx("C:/Users/Phillip Haupt/Documents/vessel_sig
 
 print("The object created and used is: boat_sightings")
 
+#read in teh gear table to be able to decode the gears used
+gear <- readxl::read_xlsx("C:/Users/Phillip Haupt/Documents/vessel_sightings/Vessel-sightings/data/gear_types_sightings.xlsx") %>% 
+  dplyr::select(gear_code = `SFC Specific Code`,
+                gear_description = `SFC Specific Description`,
+                main_gear = `Main Gear Class`)
+
 # Convert the coordinates DMds to dd and add to object boat_sightings
 source("./scripts/cost_prep_vessel_sightings_prep_convert_coordinates.R", echo=T) # currently has to be read in manually - does not run from calling source?
 # rm(d_x, d_y, dd_x, dd_y, dms_x, dms_y, m_x, m_y, s_x, s_x_chr, s_y, s_y_chr, dms_x_2, dms_y_2)
@@ -30,21 +36,29 @@ plot(sightings_utm31_sf["geometry"])#col = as.factor(sightings_utm31_sf$`Activit
 sightings_goodwin <- st_intersection(sightings_utm31_sf, goodwin_utm31_sf)
 
 # exploratory map:
-aoi <- st_buffer(goodwin_mcz, 7500)
+aoi <- st_buffer(goodwin_utm31_sf, 7500)
 
 sightings_goodwin <- sightings_goodwin %>% rename(gear = `Activity...Gear.type`)
 sightings_goodwin$gear <- as.factor(sightings_goodwin$gear)
 
+# join gear code names to gear codes
+sightings_goodwin <- left_join(sightings_goodwin, gear, by  = c('gear' = 'gear_code'))
+# convert main gear to factor for plotting purposes
+sightings_goodwin$main_gear  <- as.factor(sightings_goodwin$main_gear)
 
 # plot map
 tmap::tm_shape(aoi)+
-  tmap::tm_fill("white")+
+  tmap::tm_fill("white", alpha = 0.01)+
   tmap::tm_shape(KEIFCA)+
   tmap::tm_polygons() +
   tmap::tm_shape(goodwin_utm31_sf)+
-  tmap::tm_fill(col = "blue", alpha = 0.5) +
+  tmap::tm_fill(col = "blue", alpha = 0.3) +
   tmap::tm_shape(sightings_goodwin)+
-  tmap::tm_dots(col = "gear", palette=c(I='cyan', L1='yellow', L2='blue',O3='red',T='green'),stretch.palette = FALSE, size = 0.5, jitter = 0.1) # +
+  tmap::tm_dots(col = "main_gear", palette=c(Potting='cyan', Trawling = 'yellow', Angling = 'blue'),stretch.palette = FALSE, size = 0.1, jitter = 0.1) # +
   # tmap::tm_shape(fishing_dat_pts_goodwin_utm31_sf) +
   # tmap::tm_dots(col = "black", size = 0.5)
 #palette=c(A='cyan', B='yellow', H='blue',L='red',N='green'), 
+
+pu_sp <- as(pu_sf, 'Spatial')
+spplot(pu_sp, "cost", main = "Planning unit cost",
+       xlim = c(-0.1, 1.1), ylim = c(-0.1, 1.1))
---
+++ scripts/feature_full_names_for_plots.R
@@ -0,0 +1,8 @@
+# full feature names for plots
+library(tidyverse)
+
+
+feature_full_names <- c("Sabellaria", "Mytilus", "Subtidal sand", "Subtidal mixed sediment", "Subtidal coarse sediment", "Moderate energy circalittoral rock") %>% 
+  as_tibble() %>% rename(feature_name = value)
+feature_full_names$name <- spec$name
+feature_full_names <- left_join(feature_full_names, spec, by = "name")
